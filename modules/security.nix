006e0fc17d82845ef226b814afce5206a80bc8d7
AdRohal <kradamrohal@hotmail.com>
2026-08-05 22:42:47 +0100

    Add kernel hardening sysctl settings

--- modules/security.nix
+++ modules/security.nix
@@ -3,4 +3,30 @@
 {
   security.audit.enable = true;
   security.auditd.enable = true;
+
+  boot.kernel.sysctl = {
+    "kernel.dmesg_restrict" = 1;
+    "kernel.kptr_restrict" = 2;
+    "kernel.sysrq" = 0;
+
+    "net.ipv4.conf.all.accept_redirects" = 0;
+    "net.ipv4.conf.default.accept_redirects" = 0;
+
+    "net.ipv6.conf.all.accept_redirects" = 0;
+    "net.ipv6.conf.default.accept_redirects" = 0;
+
+    "net.ipv4.conf.all.accept_source_route" = 0;
+    "net.ipv4.conf.default.accept_source_route" = 0;
+
+    "net.ipv6.conf.all.accept_source_route" = 0;
+    "net.ipv6.conf.default.accept_source_route" = 0;
+
+    "net.ipv4.icmp_echo_ignore_broadcasts" = 1;
+    "net.ipv4.icmp_ignore_bogus_error_responses" = 1;
+
+    "net.ipv4.tcp_syncookies" = 1;
+
+    "fs.protected_symlinks" = 1;
+    "fs.protected_hardlinks" = 1;
+  };
 }
