07350d1d73a83c1a9b1f91a954995d9f4402f033
AdRohal <kradamrohal@hotmail.com>
2026-08-05 04:46:42 +0100

    Configure shell with Home Manager

--- home.nix
+++ home.nix
@@ -7,4 +7,27 @@
   home.stateVersion = "26.05";
 
   programs.home-manager.enable = true;
+
+  programs.starship = {
+    enable = true;
+  };
+
+  programs.bash = {
+    enable = true;
+
+    shellAliases = {
+      ls = "eza";
+      ll = "eza -lah";
+      cat = "bat";
+    };
+
+    initExtra = ''
+      eval "$(zoxide init bash)"
+      eval "$(starship init bash)"
+
+      if command -v fastfetch >/dev/null; then
+        fastfetch
+      fi
+    '';
+  };
 }
